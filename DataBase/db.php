d6e9eb3a464dd1415037d37bf4c6de1dd828e935
Apersant1 <maracle.on.the.turn@gmail.com>
2019-06-19 10:30:31 +0300

    change ip

--- DataBase/db.php
+++ DataBase/db.php
@@ -3,7 +3,7 @@
 require "rb.php";
 
 // R::exec( 'SET CHARACTER SET utf8' );
-R::setup( 'mysql:host=127.0.0.1;dbname=KvantLand',
+R::setup( 'mysql:host=192.168.1.13;dbname=KvantLand',
             'root','');
 
 
