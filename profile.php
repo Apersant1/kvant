3cec4ba299ad81244481b0da2678d062673f1f00
Apersant1 <maracle.on.the.turn@gmail.com>
2019-06-19 10:50:28 +0300

    fill tabs

--- profile.php
+++ profile.php
@@ -18,10 +18,11 @@ require "/DataBase/db.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/scss/profile.css">
-   
+
     <link rel="stylesheet" href="/fonts/fonts.css">
 
     <title>Профиль</title>
@@ -78,41 +79,79 @@ require "/DataBase/db.php";
 
 
 
-        <div class="container--tabs">
-	<section class="row">
-		<ul class="nav nav-tabs">
-			<li class="active"><a href="#tab-1">Проекты</a></li>
-			<li class=""><a href="#tab-2">Достижения</a></li>
-			<li class=""><a href="#tab-3">Друзья</a></li>
-		</ul>
-		<div class="tab-content">
-			<div id="tab-1" class="tab-pane active"> 
-				<span class="glyphicon glyphicon-leaf glyphicon--home--feature two columns text-center"></span>
-				<span class="col-md-10">
-					<h3>Feature 1</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				</span>
-			</div> 
-			<div id="tab-2" class="tab-pane">
-				<span class="glyphicon glyphicon-fire glyphicon--home--feature two columns text-center"></span>
-				<span class="col-md-10">
-					<h3>Feature 2</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				</span>
-			</div>
-			<div id="tab-3" class="tab-pane">
-				<span class="glyphicon glyphicon-tint glyphicon--home--feature two columns text-center"></span>
-				<span class="col-md-10">
-					<h3>Feature 3</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				</span>
-			</div>
-		</div>
-	</section>
-</div>
-     
+            <div class="container--tabs">
+                <section class="row">
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a href="#tab-1">Проекты</a></li>
+                        <li class=""><a href="#tab-2">Достижения</a></li>
+                        <li class=""><a href="#tab-3">Друзья</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div id="tab-1" class="tab-pane active">
+                            <span
+                                class="glyphicon glyphicon-leaf glyphicon--home--feature two columns text-center"></span>
+                            <span class="col-md-10">
+                                <h3>Текущие проекты</h3>
 
-            
+                                <!-- здесь блоки с проектами -->
+                                <div class="projects">
+                                    <div class="project">
+
+                                    </div>
+                                </div>
+
+                            </span>
+                        </div>
+                        <div id="tab-2" class="tab-pane">
+                            <span
+                                class="glyphicon glyphicon-fire glyphicon--home--feature two columns text-center"></span>
+                            <span class="col-md-10">
+                                <h3>Ваши достижения</h3>
+
+                                <!-- здесь блоки с ачивками -->
+                                <div class="archivments">
+                                    <div class="archivment">
+
+                                        <div class="circle-avatar"></div>
+                                        <span>God mode</span>
+                                        <nav>Выполнить 5 проектов</nav>
+
+                                    </div>
+                                    <div class="archivment">
+
+                                        <div class="circle-avatar"></div>
+                                        <span>Архиватор</span>
+                                        <nav>Не закончить 2 проекта</nav>
+
+                                    </div>
+                                    <div class="archivment">
+
+                                        <div class="circle-avatar"></div>
+                                        <span>Лучший друг</span>
+                                        <nav>Зафрендить 10 человек</nav>
+
+                                    </div>
+
+                                </div>
+                            </span>
+                        </div>
+                        <div id="tab-3" class="tab-pane">
+                            <span
+                                class="glyphicon glyphicon-tint glyphicon--home--feature two columns text-center"></span>
+                            <span class="col-md-10">
+                                <h3>Ваши друзья</h3>
+                                <!-- здесь блоки с друзьями -->
+                                <div class="friends">
+                                    <div class="friend"></div>
+                                </div>
+                            </span>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+
+
         </article>
 
 
