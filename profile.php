b9d6d074594b776232587dbad665496d144e458d
Apersant1 <maracle.on.the.turn@gmail.com>
2019-06-24 13:49:43 +0300

    add style for friend card

--- profile.php
+++ profile.php
@@ -102,15 +102,13 @@ require "/DataBase/db.php";
                                 <h1>Ваши друзья</h1>
 
                                 <div class="block-friends">
-                                    <div>1</div>
-                                    <div>2</div>
-                                    <div>3</div>
-                                    <div>4</div>
-                                    <div>5</div>
-                                    <div>6</div>
-                                    <div>7</div>
-                                    <div>8</div>
-                                    <div>9</div>
+                                   <div class="block-friend">
+                                       <div class="circle-avatar"></div>
+                                       <span></span>
+                                   </div>
+                                    
+                                
+                                
                                 </div>
 
                             </section>
